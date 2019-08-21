dfb435fbb622812bda6501a6fa461259ba822879
Santhosh-23mj <33282970+Santhosh-23mj@users.noreply.github.com>
2019-08-21 21:11:50 +0530

    Create discount.py

---
+++ WEEK 2/discount.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+cost = int(input("What is the cost ?"))
+print("The cost of the product is Rs.",cost)
+
+discounted_price = cost * 0.9
+
+print("You get that item for Rs.",discounted_price)
